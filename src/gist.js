7403967499b7b9d3b0858fb717b6bda2dd1964a0
Tim von Oldenburg <tim@voldenburg.com>
2012-10-05 11:20:11 +0200

    Initial commit

---
+++ src/gist.js
@@ -0,0 +1,28 @@
+/*
+ * gist
+ * https://github.com/tim/gist
+ *
+ * Copyright (c) 2012 Tim von Oldenburg
+ * Licensed under the MIT license.
+ */
+
+(function($) {
+
+  // Collection method.
+  $.fn.awesome = function() {
+    return this.each(function() {
+      $(this).html('awesome');
+    });
+  };
+
+  // Static method.
+  $.awesome = function() {
+    return 'awesome';
+  };
+
+  // Custom selector.
+  $.expr[':'].awesome = function(elem) {
+    return elem.textContent.indexOf('awesome') >= 0;
+  };
+
+}(jQuery));
